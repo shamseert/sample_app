3333f25b6ac13de4a8ff8bacabacb271d0fbf548
shamseer.t <shamseer.t@gmail.com>
2013-10-03 09:56:55 +0530

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,12 @@ it { should have_selector('title', 	text: 'Sign up') }
 		it "should not create a user" do
 			expect { click_button submit }.not_to change(User, :count)
 		end
+		
+
 	end
+
+
+	
 	describe "with valid information" do
 		before do
 			fill_in "Name",			with: "Example User"
@@ -32,6 +37,16 @@ it { should have_selector('title', 	text: 'Sign up') }
 			it "should create a user" do
 			expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+			describe "after saving the user" do
+			before { click_button submit }
+			let(:user) { User.find_by_email('user@example.com') }
+			it { should have_selector('title', text: user.name) }
+			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			it { should have_link('Sign out') }
+			end
 		end
+		
 	end
+	
 end
