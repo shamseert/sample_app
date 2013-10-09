cc9f5aeda5096a7df0712e8c836b62a2d646374a
shamseer.t <shamseer.t@gmail.com>
2013-10-09 16:44:44 +0530

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
 	before_save { |user| user.email = email.downcase }
 	before_save :create_remember_token
@@ -28,4 +29,9 @@ class User < ActiveRecord::Base
 
 		self.remember_token = SecureRandom.urlsafe_base64
 	end
+
+  def feed
+# This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
 	shared_examples_for "all static pages" do
 	it { should have_selector('h1',		text: heading) }
 	it { should have_selector('title', 	text: full_title(page_title)) }
-end
+	end
 
 	describe "Home page" do
 	  	before { visit root_path }
@@ -17,17 +17,33 @@ end
 
 		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Home' }
+
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
+
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+			end
+		end
+	end
 end
 
-describe "Help page" do
-	before { visit help_path }
+	describe "Help page" do
+		before { visit help_path }
 
-	let(:help)	{ 'Help' }
-	let(:page_title)	{ '' }
-end
+		let(:help)	{ 'Help' }
+		let(:page_title)	{ '' }
+	end
 
-describe "About page" do
-	before { visit about_path }
+	describe "About page" do
+		before { visit about_path }
 
 	let(:help)	{ 'About' }
 	let(:page_title)	{ '' }
@@ -38,6 +54,5 @@ describe "Contact page" do
 
 	let(:help)	{ 'Contact' }
 	let(:page_title)	{ '' }
-	
-end
+	end
 end
